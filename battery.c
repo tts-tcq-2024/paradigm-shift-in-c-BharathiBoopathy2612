0cf4c54870064c38362303005b076a3c45a82b03
BharathiBoopathy2612 <168723632+BharathiBoopathy2612@users.noreply.github.com>
2024-07-29 09:10:39 +0530

    Create battery.c

---
+++ battery.c
@@ -0,0 +1,9 @@
+#include "battery.h"
+#include "temperature.h"
+#include "soc.h"
+#include "charge_rate.h"
+
+// Function to check if battery parameters are within range
+int batteryIsOk(float temperature, float soc, float chargeRate) {
+    return isTemperatureOk(temperature) && isSocOk(soc) && isChargeRateOk(chargeRate);
+}
