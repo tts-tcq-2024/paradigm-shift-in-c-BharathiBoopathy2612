861bf85e7452ceeaef3632f3ca7257e26686f145
BharathiBoopathy2612 <168723632+BharathiBoopathy2612@users.noreply.github.com>
2024-08-04 14:41:36 +0530

    Update battery_parameters.c

--- battery_parameters.c
+++ battery_parameters.c
@@ -1,38 +1,11 @@
-#include <stdio.h>
-#include "battery_parameters.h"
+#ifndef PARAMETERS_H
+#define PARAMETERS_H
 
+// Generic constants and helper function declarations
+#define GENERIC_WARNING(message) printf("Warning: %s\n", message)
 
-static int isParameterOk(float value, float lowerLimit, float upperLimit, float warningTolerance, const char* parameterName) {
-  if (value < lowerLimit) {
-    printf("%s out of range! Too low!\n", parameterName);
-    return 0;
-  }
-  if (value > upperLimit) {
-    printf("%s out of range! Too high!\n", parameterName);
-    return 0;
-  }
-  if (value <= lowerLimit + warningTolerance) {
-    printf("Warning: Approaching low %s limit!\n", parameterName);
-  }
-  if (value >= upperLimit - warningTolerance) {
-    printf("Warning: Approaching high %s limit!\n", parameterName);
-  }
-  return 1;
-}
+// Function declarations for generic parameter checking
+int isWithinRange(float value, float lowerLimit, float upperLimit, const char* parameterName);
+int isApproachingLimit(float value, float lowerLimit, float upperLimit, float tolerance, const char* parameterName);
 
-
-int isTemperatureOk(float temperature) {
-  return isParameterOk(temperature, TEMPERATURE_LOWER_LIMIT, TEMPERATURE_UPPER_LIMIT, TEMPERATURE_WARNING_TOLERANCE, "Temperature");
-}
-
-int isSocOk(float soc) {
-  return isParameterOk(soc, SOC_LOWER_LIMIT, SOC_UPPER_LIMIT, SOC_WARNING_TOLERANCE, "State of Charge");
-}
-
-int isChargeRateOk(float chargeRate) {
-  return isParameterOk(chargeRate, 0, CHARGE_RATE_UPPER_LIMIT, CHARGE_RATE_WARNING_TOLERANCE, "Charge Rate");
-}
-
-int batteryIsOk(float temperature, float soc, float chargeRate) {
-  return isTemperatureOk(temperature) && isSocOk(soc) && isChargeRateOk(chargeRate);
-}
+#endif // PARAMETERS_H
