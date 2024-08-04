86504e1225976fd97035f38b697a239d375726f6
BharathiBoopathy2612 <168723632+BharathiBoopathy2612@users.noreply.github.com>
2024-08-04 18:28:35 +0530

    Update parameters.h

--- parameters.h
+++ parameters.h
@@ -1,12 +1,12 @@
-// parameters.h
+
 #ifndef PARAMETERS_H
 #define PARAMETERS_H
 
-// Generic constants and helper function declarations
+
 #define GENERIC_WARNING(message) printf("Warning: %s\n", message)
 
 // Function declarations for generic parameter checking
 int isWithinRange(float value, float lowerLimit, float upperLimit, const char* parameterName);
 int isApproachingLimit(float value, float lowerLimit, float upperLimit, float tolerance, const char* parameterName);
 
-#endif // PARAMETERS_H
+#endif
