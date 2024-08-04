ae616af10bd83aba09f40f9b4d760d52230a18c8
BharathiBoopathy2612 <168723632+BharathiBoopathy2612@users.noreply.github.com>
2024-08-04 14:01:00 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,16 +1,19 @@
+#include <assert.h>
 #include <stdio.h>
-
-void testIsTemperatureOk();
-void testIsSocOk();
-void testIsChargeRateOk();
-void testBatteryIsOk();
+#include "temperature.h"
+#include "soc.h"
+#include "charge_rate.h"
 
 int main() {
-    testIsTemperatureOk();
-    testIsSocOk();
-    testIsChargeRateOk();
-    testBatteryIsOk();
+  assert(isTemperatureOk(25)); // Temperature normal
+  assert(!isTemperatureOk(50)); // Temperature too high
 
-    printf("All tests passed.\n");
-    return 0;
+  assert(isSocOk(70)); // SoC normal
+  assert(!isSocOk(85)); // SoC too high
+
+  assert(isChargeRateOk(0.7)); // Charge rate normal
+  assert(!isChargeRateOk(0.9)); // Charge rate too high
+
+  printf("All tests passed!\n");
+  return 0;
 }
