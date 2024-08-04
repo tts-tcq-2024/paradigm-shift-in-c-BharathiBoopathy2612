77bf49b38dc134bc283a3fc87e6bb6a69f0f0498
BharathiBoopathy2612 <168723632+BharathiBoopathy2612@users.noreply.github.com>
2024-08-04 14:39:32 +0530

    Create temperature.c

---
+++ temperature.c
@@ -0,0 +1,9 @@
+#include "temperature.h"
+#include "parameters.h"
+
+int isTemperatureOk(float temperature) {
+  if (!isWithinRange(temperature, TEMPERATURE_LOWER_LIMIT, TEMPERATURE_UPPER_LIMIT, "Temperature")) {
+    return 0;
+  }
+  return isApproachingLimit(temperature, TEMPERATURE_LOWER_LIMIT, TEMPERATURE_UPPER_LIMIT, TEMPERATURE_WARNING_TOLERANCE, "Temperature");
+}
