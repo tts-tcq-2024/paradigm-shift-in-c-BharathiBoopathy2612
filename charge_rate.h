d3a991f44ac86b2bfca55e35f10eaf74afb1da58
BharathiBoopathy2612 <168723632+BharathiBoopathy2612@users.noreply.github.com>
2024-08-04 14:37:14 +0530

    Create charge_rate.h

---
+++ charge_rate.h
@@ -0,0 +1,10 @@
+// charge_rate.h
+#ifndef CHARGE_RATE_H
+#define CHARGE_RATE_H
+
+#define CHARGE_RATE_UPPER_LIMIT 0.8
+#define CHARGE_RATE_WARNING_TOLERANCE 0.04
+
+int isChargeRateOk(float chargeRate);
+
+#endif // CHARGE_RATE_H
