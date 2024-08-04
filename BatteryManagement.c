1c6a34195f461ec38218a6a82059178bc758b2f1
BharathiBoopathy2612 <168723632+BharathiBoopathy2612@users.noreply.github.com>
2024-08-04 14:13:46 +0530

    Delete BatteryManagement.c

--- BatteryManagement.c
+++
@@ -1,9 +0,0 @@
-#include "BatteryManagement.h"
-#include "temperature.h"
-#include "soc.h"
-#include "charge_rate.h"
-
-// Function to check if battery parameters are within range
-int batteryIsOk(float temperature, float soc, float chargeRate) {
-    return isTemperatureOk(temperature) && isSocOk(soc) && isChargeRateOk(chargeRate);
-}
