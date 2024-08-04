40d6251b01b798694560261c96a2a1f3df0b6e55
BharathiBoopathy2612 <168723632+BharathiBoopathy2612@users.noreply.github.com>
2024-08-04 15:04:32 +0530

    Update parameters.c

--- parameters.c
+++ parameters.c
@@ -1,11 +1,26 @@
-#ifndef PARAMETERS_H
-#define PARAMETERS_H
+// parameters.c
+#include <stdio.h>
+#include "parameters.h"
 
+// Check if a value is within the specified range
+int isWithinRange(float value, float lowerLimit, float upperLimit, const char* parameterName) {
+  if (value < lowerLimit) {
+    printf("%s out of range! Too low!\n", parameterName);
+    return 0;
+  }
+  if (value > upperLimit) {
+    printf("%s out of range! Too high!\n", parameterName);
+    return 0;
+  }
+  return 1;
+}
 
-#define GENERIC_WARNING(message) printf("Warning: %s\n", message)
-
-
-int isWithinRange(float value, float lowerLimit, float upperLimit, const char* parameterName);
-int isApproachingLimit(float value, float lowerLimit, float upperLimit, float tolerance, const char* parameterName);
-
-#endif 
+// Check if a value is approaching a limit within a specified tolerance
+int isApproachingLimit(float value, float lowerLimit, float upperLimit, float tolerance, const char* parameterName) {
+  if (value <= lowerLimit + tolerance) {
+    GENERIC_WARNING("Approaching low limit");
+  } else if (value >= upperLimit - tolerance) {
+    GENERIC_WARNING("Approaching high limit");
+  }
+  return 1;
+}
