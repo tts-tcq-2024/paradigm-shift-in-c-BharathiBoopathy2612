f096d7a88f7b84b528fe43638b2355d01b6069e5
BharathiBoopathy2612 <168723632+BharathiBoopathy2612@users.noreply.github.com>
2024-08-04 14:14:52 +0530

    Delete testcase_chargerate.c

--- testcase_chargerate.c
+++
@@ -1,9 +0,0 @@
-#include "charge_rate.h"
-#include <assert.h>
-
-// Test cases for isChargeRateOk function
-void testIsChargeRateOk() {
-    assert(isChargeRateOk(0.5) == 1);     // In range
-    assert(isChargeRateOk(0.9) == 0);     // Above range
-}
-
