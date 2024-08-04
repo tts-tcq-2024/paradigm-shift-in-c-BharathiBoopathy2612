1a5ff94adc0af386f7d0f918d34c481ef2d72329
BharathiBoopathy2612 <168723632+BharathiBoopathy2612@users.noreply.github.com>
2024-08-04 14:24:45 +0530

    Update battery_parameters.c

--- battery_parameters.c
+++ battery_parameters.c
@@ -1,8 +1,8 @@
-// battery_parameters.c
+
 #include <stdio.h>
 #include "battery_parameters.h"
 
-// Generic function to check if a parameter is within acceptable range and provide warnings
+
 static int isParameterOk(float value, float lowerLimit, float upperLimit, float warningTolerance, const char* parameterName) {
   if (value < lowerLimit) {
     printf("%s out of range! Too low!\n", parameterName);
@@ -21,7 +21,7 @@ static int isParameterOk(float value, float lowerLimit, float upperLimit, float 
   return 1;
 }
 
-// Wrapper functions for specific parameter checks
+
 int isTemperatureOk(float temperature) {
   return isParameterOk(temperature, TEMPERATURE_LOWER_LIMIT, TEMPERATURE_UPPER_LIMIT, TEMPERATURE_WARNING_TOLERANCE, "Temperature");
 }
@@ -34,7 +34,6 @@ int isChargeRateOk(float chargeRate) {
   return isParameterOk(chargeRate, 0, CHARGE_RATE_UPPER_LIMIT, CHARGE_RATE_WARNING_TOLERANCE, "Charge Rate");
 }
 
-// Function to check if the battery is overall OK by checking all parameters
 int batteryIsOk(float temperature, float soc, float chargeRate) {
   return isTemperatureOk(temperature) && isSocOk(soc) && isChargeRateOk(chargeRate);
 }
