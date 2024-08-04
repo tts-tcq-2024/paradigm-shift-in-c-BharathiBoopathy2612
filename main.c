4cec516f695ecb58e44d49176c80074c3818d9b6
BharathiBoopathy2612 <168723632+BharathiBoopathy2612@users.noreply.github.com>
2024-08-04 14:19:21 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,12 @@
+#include <assert.h>
 
+int main()
+{
+  assert(batteryIsOk(25, 70, 0.7)); 
+  assert(!batteryIsOk(50, 85, 0));  
+  assert(batteryIsOk(2, 22, 0.7));  
+  assert(batteryIsOk(43, 75, 0.76)); 
+  
+  printf("All tests passed!\n");
+  return 0;
+}
