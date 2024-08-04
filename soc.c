c1bb34db84b010837e31aea0c318b7a1de81badb
BharathiBoopathy2612 <168723632+BharathiBoopathy2612@users.noreply.github.com>
2024-08-04 14:51:27 +0530

    Update soc.c

--- soc.c
+++ soc.c
@@ -1,5 +1,5 @@
 #include "soc.h"
-#include "parameters.h"
+#include "battery_parameters.h"
 
 int isSocOk(float soc) {
   if (!isWithinRange(soc, SOC_LOWER_LIMIT, SOC_UPPER_LIMIT, "State of Charge")) {
