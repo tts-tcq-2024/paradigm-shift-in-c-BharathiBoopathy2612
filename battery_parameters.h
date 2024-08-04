33478fd62d8e420cb8b2e48a40587ad16fe8b5e9
BharathiBoopathy2612 <168723632+BharathiBoopathy2612@users.noreply.github.com>
2024-08-04 14:22:29 +0530

    Update battery_parameters.h

--- battery_parameters.h
+++ battery_parameters.h
@@ -1,1 +1,22 @@
+#ifndef BATTERY_PARAMETERS_H
+#define BATTERY_PARAMETERS_H
 
+// Constants defining the normal operating ranges and tolerance for warnings
+#define TEMPERATURE_LOWER_LIMIT 0
+#define TEMPERATURE_UPPER_LIMIT 45
+#define TEMPERATURE_WARNING_TOLERANCE 2.25
+
+#define SOC_LOWER_LIMIT 20
+#define SOC_UPPER_LIMIT 80
+#define SOC_WARNING_TOLERANCE 4
+
+#define CHARGE_RATE_UPPER_LIMIT 0.8
+#define CHARGE_RATE_WARNING_TOLERANCE 0.04
+
+
+int isTemperatureOk(float temperature);
+int isSocOk(float soc);
+int isChargeRateOk(float chargeRate);
+int batteryIsOk(float temperature, float soc, float chargeRate);
+
+#endif
