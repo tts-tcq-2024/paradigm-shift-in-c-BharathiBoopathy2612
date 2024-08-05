ba856698752ddab5d822191f2257feb8fad3fd0a
BharathiBoopathy2612 <168723632+BharathiBoopathy2612@users.noreply.github.com>
2024-08-05 09:36:38 +0530

    Delete temperature.c

--- temperature.c
+++
@@ -1,9 +0,0 @@
-#include "temperature.h"
-#include "parameters.h"
-
-int isTemperatureOk(float temperature) {
-  if (!isWithinRange(temperature, TEMPERATURE_LOWER_LIMIT, TEMPERATURE_UPPER_LIMIT, "Temperature")) {
-    return 0;
-  }
-  return isApproachingLimit(temperature, TEMPERATURE_LOWER_LIMIT, TEMPERATURE_UPPER_LIMIT, TEMPERATURE_WARNING_TOLERANCE, "Temperature");
-}
