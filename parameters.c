ff11a92d86fadf0f2e2aa06f1b34d0b7ba14c379
BharathiBoopathy2612 <168723632+BharathiBoopathy2612@users.noreply.github.com>
2024-08-05 09:25:26 +0530

    Update parameters.c

--- parameters.c
+++ parameters.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
-#include "parameters.h"
-
+#include "battery_parameters.h"
+#include "message_translations.h"
 
 int isWithinRange(float value, float lowerLimit, float upperLimit, const char* parameterName) {
-  if (value < lowerLimit) {
-    printf("%s out of range! Too low!\n", parameterName);
-    return 0;
-  }
-  if (value > upperLimit) {
-    printf("%s out of range! Too high!\n", parameterName);
-    return 0;
-  }
-  return 1;
+    if (value < lowerLimit) {
+        printf("%s\n", translateMessage("out of range! Too low!"));
+        return 0;
+    }
+    if (value > upperLimit) {
+        printf("%s\n", translateMessage("out of range! Too high!"));
+        return 0;
+    }
+    return 1;
 }
 
 int isApproachingLimit(float value, float lowerLimit, float upperLimit, float tolerance, const char* parameterName) {
-  if (value <= lowerLimit + tolerance) {
-    GENERIC_WARNING("Approaching low limit");
-  } else if (value >= upperLimit - tolerance) {
-    GENERIC_WARNING("Approaching high limit");
-  }
-  return 1;
+    if (value <= lowerLimit + tolerance) {
+        printf("%s: %s\n", translateMessage("Warning"), translateMessage("Approaching low limit"));
+    } else if (value >= upperLimit - tolerance) {
+        printf("%s: %s\n", translateMessage("Warning"), translateMessage("Approaching high limit"));
+    }
+    return 1;
 }
