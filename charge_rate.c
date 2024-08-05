96ef072cefc81b9b0e6dc8d1f120b445b22dfd14
BharathiBoopathy2612 <168723632+BharathiBoopathy2612@users.noreply.github.com>
2024-08-05 09:39:58 +0530

    Update charge_rate.c

--- charge_rate.c
+++ charge_rate.c
@@ -1,9 +1,10 @@
-#include "charge_rate.h"
-#include "parameters.h"
+#include "charge_rate_check.h"
+#include "battery_parameters.h"
+#include "message_translations.h"
 
-int isChargeRateOk(float chargeRate) {
-  if (!isWithinRange(chargeRate, 0, CHARGE_RATE_UPPER_LIMIT, "Charge Rate")) {
-    return 0;
-  }
-  return isApproachingLimit(chargeRate, 0, CHARGE_RATE_UPPER_LIMIT, CHARGE_RATE_WARNING_TOLERANCE, "Charge Rate");
+int validateChargeRate(float chargeRate) {
+    if (!checkValueInRange(chargeRate, 0, CHARGE_RATE_MAX_LIMIT, translateMessage("Charge Rate"))) {
+        return 0;
+    }
+    return checkWarningLevel(chargeRate, 0, CHARGE_RATE_MAX_LIMIT, CHARGE_RATE_WARNING_TOLERANCE, translateMessage("Charge Rate"));
 }
